d8c962e4584cf9960cab7dc30f0577dfb9570240
Adelise F <Adeliseferris@gmail.com>
2025-08-27 12:00:33 -0700

    Add Product class with validation attributes (#13)
    Introduces a new `Product` class in the `eCommerce.Models` namespace.
    The class includes properties for `ProductId`, `Title`, and `Price`, with
    validation attributes for primary key, required title length, and price range.
    XML documentation comments are also added for clarity.

---
+++ eCommerce/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models;
+
+/// <summary>
+/// Represents an idividual product for sale
+/// </summary>
+
+public class Product
+{
+    [Key]
+    /// <summary>
+    /// The unique identifier for the product
+    /// </summary>
+
+
+    public int ProductId { get; set; }
+
+    /// <summary>
+    /// The user facing title of the product
+    /// </summary>
+
+    [StringLength(50, ErrorMessage = "Title cannot be more than 50 characters")]
+
+    public required string Title { get; set; }
+
+    /// <summary>
+    /// The current sales price of the product
+    /// </summary>
+
+    [Range(0, 10_000)]
+
+    public double Price { get; set; }
+}
+
