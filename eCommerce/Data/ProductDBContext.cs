288593ba4c2b6815786543baead5be92dd1b855d
Adelisef33 <AdeliseFerris@gmail.com>
2025-08-27 12:22:52 -0700

    Add ProductDBContext for Entity Framework integration
    Introduced a new `ProductDBContext` class inheriting from `DBContext`.
    This class includes a constructor for `DbContextOptions` and a
    `DbSet<Product>` property to manage the collection of `Product`
    entities within the context.

---
+++ eCommerce/Data/ProductDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data
+
+public class ProductDBContext : DBContext
+{
+    public ProductDBContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    // Entities to be tracked by DBContext
+    public DbSet<Product> Products { get; set; }
+}
+
